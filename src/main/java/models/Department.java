b9186148e64095be88d3ecacbaa7551fc54bd84a
Mumo <mnzinga888@gmail.com>
2020-05-13 22:04:36 +0300

    add department

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.util.Objects;
+
+public class Department {
+    private String name;
+    private String description;
+    private int id;
+
+    public Department(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department department = (Department) o;
+        return id == department.id &&
+                Objects.equals(name, department.name)&&
+                Objects.equals(description, department.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id);
+    }
+
+}
